a0203f78fdf1174fcee876aca930984d1b657c1c
Phil Jirsa <phil.jirsa@microsoft.com>
2023-04-28 12:51:54 -0500

    Initial commit :circus_tent:

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            // Test with native app trace log - this WILL stream to event hub when configured
+            //System.Diagnostics.Trace.TraceError("If you're seeing this, we did some things");
+            
+            // Use log4net to log to a file - this will NOT stream to event hub
+            log4net.LogManager.GetLogger("DemoSession").Info("Hello from values controller!");
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
